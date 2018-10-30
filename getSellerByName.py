4dd4d81adf5fd5b2865396ef362bcce16cfcacd7
yunhe <cloudycrane@hotmail.com>
2018-10-30 18:50:36 +0800

    add header in getSellerByName.py

--- getSellerByName.py
+++ getSellerByName.py
@@ -4,6 +4,19 @@ import urllib2
 import sys
 from lxml import etree
 
+def url_get(url):
+    # print('GET ' + url)
+    header = dict()
+    header['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    header['Accept-Encoding'] = 'gzip,deflate,sdch'
+    header['Accept-Language'] = 'en-US,en;q=0.8'
+    header['Connection'] = 'keep-alive'
+    header['DNT'] = '1'
+    request = urllib2.Request(url, headers=header)
+    response = urllib2.urlopen(request)
+    header['User-Agent'] = 'Mozilla/12.0 (compatible; MSIE 8.0; Windows NT)'
+    return response.read().decode('utf-8')
+
 
 def getSellerByName(seller):
     reload(sys)
@@ -12,10 +25,7 @@ def getSellerByName(seller):
     URL_BASE = 'https://shop.m.taobao.com/shop/shop_search.htm?q={}'
     url = URL_BASE.format(quote_plus(seller))
 
-    header = {}
-    request = urllib2.Request(url, headers=header)
-    response = urllib2.urlopen(request)
-    html = response.read().decode('utf-8')
+    html = url_get(url)
 
     tree = etree.HTML(html, parser=etree.HTMLParser(encoding='utf-8'))
     tables = tree.xpath('//div[@class="detail"]/table')
@@ -33,7 +43,6 @@ def getSellerByName(seller):
             "products": products
         }
     return(res)
-
 
 
 if __name__ == '__main__':
